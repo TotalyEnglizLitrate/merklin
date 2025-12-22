8d027be8f9bf09df3c17b49d5c4e8e95ca9a7145
Narendra S <narendra.s1232@gmail.com>
2025-12-22 20:14:10 +0530

    merkle: Skeletal structure of MerkleTree and LogNode classes

---
+++ merkle-tree/src/merkle_tree/__init__.py
@@ -0,0 +1,20 @@
+from hashlib import sha256
+
+class LogNode:
+    def __init__(self, log: str) -> None:
+        self.log = sha256(log.encode()).hexdigest()
+
+class MerkleTree:
+    def __init__(self) -> None:
+        self.leaves: list[LogNode] = []
+        self.root: str | None = None
+
+    def add_log(self, log: str) -> None:
+        node = LogNode(log)
+        self.leaves.append(node)
+        
+    def membership_proof(self, log: str) -> list[str]:
+        raise NotImplementedError("Membership proof not implemented")
+    
+    def consistency_proof(self, p1: int, p2: int) -> list[str]:
+        raise NotImplementedError("Consistency proof not implemented")
